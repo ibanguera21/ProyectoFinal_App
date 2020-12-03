020c707e878987b49999da35d6096007e7818168
ibanguera21 <72167529+ibanguera21@users.noreply.github.com>
2020-12-03 10:46:03 -0500

    Update and rename src/environments/environment.ts to src/ambientes/environment.ts

--- src/ambientes/environment.ts
+++ src/ambientes/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyD5NJO69WBV3qNC2bPXmHHFLLcKiFmd7Dc",
-    authDomain: "internalsecurity-6ac0c.firebaseapp.com",
-    databaseURL: "https://internalsecurity-6ac0c.firebaseio.com",
-    projectId: "internalsecurity-6ac0c",
-    storageBucket: "internalsecurity-6ac0c.appspot.com",
-    messagingSenderId: "508535294515",
-    appId: "1:508535294515:web:ff696e973ff75476a3f146",
-    measurementId: "G-NDG16Q917Q"}
+  production: false
   
 };
 
